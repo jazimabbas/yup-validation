a2799af6bc7579e611c113dbb5b7edf4dda521bc
Jazim Abbas <jazimabbas87@gmail.com>
2023-05-23 14:00:57 +0500

    🚦 Added middleware for unhandled exceptions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+require("express-async-errors");
+const unhandleExceptions = require("./middlewares/unhandle-exceptions");
 
 const app = express();
+app.use(express.json());
 app.get("/", (_, res) => res.json({ message: "YUP Validation!" }));
+app.use(unhandleExceptions);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App is listening on port ${PORT} ...`));
---
+++ middlewares/unhandle-exceptions.js
@@ -0,0 +1,20 @@
+const Errors = require("../libs/custom-errors");
+
+module.exports = (err, _, res, next) => {
+  let statusCode = 500;
+  let message = "Server Error";
+  let errors = [];
+
+  if (err instanceof Errors.HttpError) {
+    statusCode = err.statusCode;
+    message = err.message;
+
+    if (err instanceof Errors.ValidationError) {
+      errors = err.errors;
+    }
+  }
+
+  res.status(statusCode).json({ message, errors });
+
+  next();
+};
